fb3152c60b4ec99c5c6dc41be22266f38603b997
Mikhail Kolyushin <kolyushin.m@yandex.ru>
2024-11-05 02:03:22 +0500

    Добавлен класс для проверки ручки изменения пользователя

---
+++ src/test/java/praktikum/user/EditUserTest.java
@@ -0,0 +1,40 @@
+package praktikum.user;
+
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Test;
+
+public class EditUserTest {
+    private UserClient client = new UserClient();
+    private UserChecks check = new UserChecks();
+
+    String accessToken;
+
+    @After
+    public void deleteUser(){
+        if(accessToken != null){
+            ValidatableResponse response = client.deleteUser(accessToken);
+            check.deleteUser(response);
+        }
+    }
+
+    @Test
+    @DisplayName("Изменение данных пользователя с авторизацией")
+    public void successEditUserTest(){
+        var user = User.randomUser();
+        ValidatableResponse createResponse = client.createUser(user);
+        check.checkCreated(createResponse);
+
+        var creds = UserCredentionals.fromUser(user);
+        ValidatableResponse loginResponse = client.loginUser(creds);
+        accessToken = check.checkLoggedIn(loginResponse);
+
+        ValidatableResponse getUserResponse = client.getUserData(accessToken);
+        check.checkGetUserData(getUserResponse);
+
+        ValidatableResponse editResponse = client.editUser(accessToken);
+        check.checkEditUser(editResponse);
+
+    }
+}
